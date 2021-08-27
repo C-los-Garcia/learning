518d88315a5ed12563f75f3d6c783c8abf330e40
C-los-Garcia <89553107+C-los-Garcia@users.noreply.github.com>
2021-08-27 10:24:03 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+// whole line comment 
+basic.showIcon(IconNames.Heart)   // this is also me trying to learn to do a partial line comment
+
+/*
+    this is me trying to learn how to block comment
+*/
+  
+  
